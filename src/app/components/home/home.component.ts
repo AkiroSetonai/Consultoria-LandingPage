09f25b529d06303a57cafe110b6b5107000a9c0f
AkiroSetonai <wallyson.cavalcante@gmail.com>
2025-04-07 18:12:05 -0300

    First Commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { NgOptimizedImage } from '@angular/common';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [HeaderComponent, NgOptimizedImage, BtnPrimaryComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
